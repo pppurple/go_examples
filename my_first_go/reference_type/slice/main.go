897c889b2ba4798c8556d8f65101aecffa733c2c
pppurple <shuggie.otis0@gmail.com>
2018-03-23 02:02:43 +0900

    add slice example

--- my_first_go/reference_type/slice/main.go
+++ my_first_go/reference_type/slice/main.go
@@ -36,4 +36,118 @@ func main() {
 	// string
 	str := "ABCDEF"[1:3]
 	fmt.Println(str)
+
+	// append
+	sa := []int{1, 2, 3}
+	sa = append(sa, 4)
+	fmt.Println(sa)
+	sa = append(sa, 5, 6, 7)
+	fmt.Println(sa)
+
+	sa1 := []int{1, 2, 3}
+	sa2 := []int{4, 5, 6}
+	sa3 := append(sa1, sa2...)
+	fmt.Println(sa3)
+
+	// auto expand slice size
+	autoExpand()
+
+	// copy
+	copySlice()
+
+	// full slice expressions
+	fullSliceExpression()
+
+	// loop
+	sliceLoop()
+
+	// variable args
+	variableArgs()
+
+	// reference type
+	arr := [3]int{1, 2, 3}
+	double(arr) // call by value
+	fmt.Println(arr)
+	sli := []int{1, 2, 3}
+	doubleRef(sli) // call by reference
+	fmt.Println(sli)
+
+}
+
+func autoExpand() {
+	s := make([]int, 0, 0)
+	fmt.Printf("len=%d, cap=%d\n", len(s), cap(s))
+	s = append(s, 1)
+	fmt.Printf("len=%d, cap=%d\n", len(s), cap(s))
+	s = append(s, []int{2, 3, 4}...)
+	fmt.Printf("len=%d, cap=%d\n", len(s), cap(s))
+	s = append(s, 5)
+	fmt.Printf("len=%d, cap=%d\n", len(s), cap(s))
+	s = append(s, 6, 7, 8, 9)
+	fmt.Printf("len=%d, cap=%d\n", len(s), cap(s))
+}
+
+func copySlice() {
+	s1 := []int{1, 2, 3, 4, 5}
+	s2 := []int{10, 11}
+	n := copy(s1, s2)
+	fmt.Println(s1)
+	fmt.Println(n)
+}
+
+func fullSliceExpression() {
+	a := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	s1 := a[2:4]
+	fmt.Println(len(s1))
+	fmt.Println(cap(s1))
+
+	s2 := a[2:4:4]
+	fmt.Println(len(s2))
+	fmt.Println(cap(s2))
+
+	s3 := a[2:4:6]
+	fmt.Println(len(s3))
+	fmt.Println(cap(s3))
+}
+
+func sliceLoop() {
+	s := []string{"apple", "banana", "cherry"}
+
+	for i, v := range s {
+		fmt.Printf("[%d] => %s\n", i, v)
+	}
+}
+
+func variableArgs() {
+	fmt.Println(sum(1, 2, 3))
+	fmt.Println(sum(1, 2, 3, 4, 5))
+	fmt.Println(sum())
+
+	s := []int{2, 4, 6}
+	fmt.Println(sum(s...))
+}
+
+func sum(s ...int) int {
+	n := 0
+	for _, v := range s {
+		n += v
+	}
+	return n
+}
+
+// call by value
+func double(a [3]int) {
+	for i, v := range a {
+		a[i] = 2 * v
+	}
+	return
+}
+
+// call by reference
+func doubleRef(a []int) {
+	for i, v := range a {
+		a[i] = 2 * v
+	}
+	return
 }

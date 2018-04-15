92fc731f9c4d6c076ee9646df0fb6612b2782e14
pppurple <shuggie.otis0@gmail.com>
2018-04-16 02:57:54 +0900

    [WIP] add command line example

---
+++ command_line_example/myls/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+// -l format long
+// -a all
+// -t sort time
+// -r reverse
+// -h
+const version = "1.0"
+
+func main() {
+	// get current dir
+	// dir, err := os.Getwd()
+
+	var showDetail bool
+	flag.BoolVar(&showDetail, "l", false, "show list with more detail.")
+	var showVersion bool
+	flag.BoolVar(&showVersion, "v", false, "show version.")
+	if showVersion {
+		fmt.Println("version: " + version)
+		return
+	}
+
+	f, err := os.Open(".")
+	if err != nil {
+		log.Fatal("Please input dir name:")
+	}
+	defer f.Close()
+
+	fis, err := f.Readdir(0)
+	if err != nil {
+		log.Fatal("Please input dir name:")
+	}
+	ls(fis)
+	for _, fi := range fis {
+		fmt.Println(fi.Name())
+		fmt.Println(fi.Size())
+		fmt.Println(fi.Mode())
+		fmt.Println(fi.ModTime())
+		fmt.Println(fi.IsDir())
+		fmt.Println(fi.Sys())
+
+		if fi.IsDir() {
+			// fmt.Println(fi.Name())
+		}
+	}
+}
+
+func ls(fis []os.FileInfo) {
+	for _, fi := range fis {
+		fmt.Print(fi.Name() + " ")
+	}
+	fmt.Println("")
+}
+
+func longFormat(fis []os.FileInfo) {
+	for _, fi := range fis {
+		fmt.Print(fi.Name() + " ")
+		fmt.Println("")
+	}
+}
